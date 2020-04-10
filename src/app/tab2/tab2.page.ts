0164b3e5ac81a8d326e5302e769ee26122a79cbf
edpinck03w <edward.pinckney@gmail.com>
2020-04-10 18:48:00 -0400

    class 2 changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Message } from '../models/message';
+import { SharedService } from '../services/shared.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  message: Message = new Message();
 
+  constructor( private shared: SharedService, private data: DataService) {}
+
+  onPost() {
+    this.message.from = this.shared.userName;
+    this.message.createdOn = new Date();
+    this.data.saveMessage(this.message);
+
+    console.log('posting', this.message);
+
+    //clear the form after each submit
+    this.message = new Message();
+  }
 }
